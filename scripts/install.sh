25115c190a2061e88aff104cc7978d8614a5a5a8
Sergei Rams <ser.rams.243@gmail.com>
2023-03-20 09:44:20 +0000

    add zsh and powerlevel10k zsh theme

---
+++ scripts/install.sh
@@ -0,0 +1,49 @@
+sudo apt update && sudo apt full-upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+install curl
+install git
+install htop
+install jq
+
+for f in programs/*.sh; do bash "$f" -H; done
+
+
+dotfilesDir=$(pwd)
+
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink 
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest} 
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+linkDotfile .p10k.zsh
+linkDotfile .zshrc
---
+++ scripts/programs/zsh.sh
@@ -0,0 +1,3 @@
+sudo apt install -y zsh
+
+sudo chsh -s $(which zsh) $(whoami)
