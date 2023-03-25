760f927bf924aceb9012e6df57ac066bc066078e
Sergei Rams <ser.rams.243@gmail.com>
2023-03-25 17:34:10 +0000

    set nvim as default git editor

--- scripts/programs/nvim.sh
+++ scripts/programs/nvim.sh
@@ -1,3 +1,5 @@
 curl -LS https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb -o /tmp/nvim.deb
 sudo apt install /tmp/nvim.deb
 rm /tmp/nvim.deb
+
+git config --global core.editor "nvim"
