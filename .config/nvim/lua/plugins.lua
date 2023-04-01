4efd617f978698527a8815e9b68667575d7f9c60
auvred <ser.rams.243@gmail.com>
2023-04-01 21:32:45 +0000

    rewrite nvim config in lua
    move from vim-plug to packer.nvim
    move from nerdtree to nvimtree

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,44 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+
+  use {'neoclide/coc.nvim', branch = 'release'}
+  vim.cmd "let g:coc_global_extensions = ['coc-json', 'coc-emmet', 'coc-eslint', 'coc-tsserver']"
+
+  use 'navarasu/onedark.nvim'
+
+
+  use 'leafgarland/typescript-vim'
+
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end
+  }
+  vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+      if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+        vim.cmd "quit"
+      end
+    end
+  })
+
+
+  use {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup {
+        timeout = 200
+      }
+    end,
+  }
+
+end)
---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,25 @@
+local cmd = vim.cmd
+local opt = vim.opt
+
+
+cmd('syntax enable')
+opt.termguicolors = true
+cmd('colorscheme onedark')
+
+-- highlight yanked text
+vim.api.nvim_exec([[
+augroup YankHighlight
+autocmd!
+autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=200}
+augroup end
+]], false)
+
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.wrap = true
+opt.tabstop = 2
+opt.number = true
+opt.cursorline = true
+opt.relativenumber = true
+opt.scrolloff = 10
+
