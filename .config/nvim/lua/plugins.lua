af0057d7fe7f931aad0212119b36e04d06946d5e
auvred <ser.rams.243@gmail.com>
2023-04-01 22:28:45 +0000

    add lualine setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -44,8 +44,10 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup()
+    end
   }
-  require('lualine').setup()
 
 end)
