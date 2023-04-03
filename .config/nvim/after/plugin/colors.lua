f10dd396144ad5636e895e56bdbb7fd0b2f85370
auvred <ser.rams.243@gmail.com>
2023-04-03 20:15:43 +0000

    update nvim theme colors

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,4 +1,9 @@
+-- https://github.com/navarasu/onedark.nvim/blob/master/lua/onedark/highlights.lua
 require('onedark').setup {
-  style = 'warmer'
+  style = 'warmer',
+  highlights = {
+    ["@tag"] = { fg = '#5bb576' },
+    ["@tag.delimiter"] = { fg = '#c9a157' },
+  }
 }
 require('onedark').load()
