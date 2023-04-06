1952e1ba8419561b7731f677aa5d63e3066d3fd6
auvred <ser.rams.243@gmail.com>
2023-04-06 08:08:45 +0000

    show files ignored by git in nvim-tree

---
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,15 @@
+require('nvim-tree').setup {
+  git = {
+    ignore = false
+  }
+}
+
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require('nvim-tree.utils').is_nvim_tree_buf() then
+      vim.cmd 'quit'
+    end
+  end
+})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -17,18 +17,7 @@ return require('packer').startup(function(use)
     requires = {
       'nvim-tree/nvim-web-devicons',
     },
-    config = function()
-      require('nvim-tree').setup {}
-    end
   }
-  vim.api.nvim_create_autocmd('BufEnter', {
-    nested = true,
-    callback = function()
-      if #vim.api.nvim_list_wins() == 1 and require('nvim-tree.utils').is_nvim_tree_buf() then
-        vim.cmd 'quit'
-      end
-    end
-  })
 
 
   use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
