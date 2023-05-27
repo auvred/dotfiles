174414414951ef888f29e9c0042c43d7f6911d69
auvred <aauvred@gmail.com>
2023-05-27 23:41:16 +0000

    add some git aliases

--- scripts/programs/nvim.sh
+++ scripts/programs/nvim.sh
@@ -15,5 +15,3 @@ nvim --headless -c 'TSUpdateSync' -c q
 mkdir -p ~/.config/coc/extensions
 echo '{"dependencies": {}}' >~/.config/coc/extensions/package.json
 npm install --install-strategy=shallow --ignore-scripts --no-bin-links --no-package-lock --omit=dev -C ~/.config/coc/extensions $(nvim --headless -c'echo g:coc_global_extensions | quit' 2>&1 | tail -n1 | sed "s=\(', '\|\['\|']\)=\n=g")
-
-git config --global core.editor "nvim"
