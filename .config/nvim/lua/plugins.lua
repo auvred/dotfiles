3d19f5ec26a3a3bdde8e9f9e8668f8dd277e1af8
auvred <aauvred@gmail.com>
2023-08-05 20:35:03 +0000

    add template-string nvim plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -76,4 +76,17 @@ require("lazy").setup({
 			require("Comment").setup()
 		end,
 	},
+  {
+    'axelvc/template-string.nvim',
+    opts = {
+      filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue' },
+      remove_template_string = true,
+      restore_quotes = {
+        normal = [[']],
+        jsx = [["]],
+      },
+    },
+    event = 'InsertEnter',
+    ft = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue' },
+  },
 })
