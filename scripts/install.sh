b48ee08113847524c6dac5b56a4790d7541a14f8
auvred <aauvred@gmail.com>
2023-08-05 20:40:59 +0000

    add name and email for git in ~/.dotfiles

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,23 +14,20 @@ sudo apt install -y \
   fd-find \
   wget
 
-# this was stolen from here https://github.com/tomnomnom/dotfiles/blob/d2f90b12081f3d4364795a834df7fa8890aed6cd/setup.sh
+# source: https://github.com/tomnomnom/dotfiles/blob/d2f90b12081f3d4364795a834df7fa8890aed6cd/setup.sh
 function linkDotfile {
   dest="${HOME}/${1}"
   dateStr=$(date +%Y-%m-%d-%H%M)
 
   if [ -h ~/${1} ]; then
-    # Existing symlink 
     echo "Removing existing symlink: ${dest}"
     rm ${dest} 
 
   elif [ -f "${dest}" ]; then
-    # Existing file
     echo "Backing up existing file: ${dest}"
     mv ${dest}{,.${dateStr}}
 
   elif [ -d "${dest}" ]; then
-    # Existing dir
     echo "Backing up existing dir: ${dest}"
     mv ${dest}{,.${dateStr}}
   fi
@@ -75,3 +72,8 @@ for p in ${programs[@]}; do
     echo "Script $file_path not found. Skipping..."
   fi
 done
+
+cd $dotfiles_dir
+git config user.name auvred
+git config user.email aauvred@gmail.com
+cd -
