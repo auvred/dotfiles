1cda89d1bf9f01c922dc32dad441e83d6df86a3d
auvred <aauvred@gmail.com>
2023-08-12 16:17:35 +0000

    add neoscroll.nvim, bump plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -108,4 +108,19 @@ require("lazy").setup({
       "vue",
     },
   },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({mappings = {'<C-u>', '<C-d>', 'zt', 'zz', 'zb'}})
+      
+      local t = {}
+      t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}}
+      t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '100'}}
+      t['zt']    = {'zt', {'250'}}
+      t['zz']    = {'zz', {'250'}}
+      t['zb']    = {'zb', {'250'}}
+      
+      require('neoscroll.config').set_mappings(t)
+    end
+  },
 })
