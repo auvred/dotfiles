c0fea442d2cfbf5c6a5eedcd98b04e85687113c3
auvred <aauvred@gmail.com>
2023-08-12 15:32:22 +0000

    fix formatting

--- scripts/programs/nvim.sh
+++ scripts/programs/nvim.sh
@@ -4,7 +4,7 @@ if [[ $(command -v nvim) ]]; then
   # Slim won't have coc.nvim :(
   if [[ $MY_WORKSPACE_TYPE != dev-container-slim ]]; then
     mkdir -p ~/.config/coc/extensions
-    
+
     # TODO: remove after https://github.com/neoclide/coc-tsserver/issues/434 will be resolved
     # i just don't want to download bloated typescript package twice for slim container
     packagejson='{
@@ -19,7 +19,7 @@ if [[ $(command -v nvim) ]]; then
       -c'echo g:coc_global_extensions | quit' 2>&1 \
       | tail -n1 \
       | sed "s=\(', '\|\['\|']\)=\n=g")
-    
+
     npm install \
       --ignore-scripts \
       --no-bin-links \
