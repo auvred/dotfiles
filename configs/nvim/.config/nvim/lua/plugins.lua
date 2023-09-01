ef12c01d4cca528da6904eecd03aec93c8ca24ca
auvred <aauvred@gmail.com>
2023-09-01 16:57:38 +0000

    add keymappings for telescope.nvim + bump plugin versions

--- configs/nvim/.config/nvim/lua/plugins.lua
+++ configs/nvim/.config/nvim/lua/plugins.lua
@@ -72,6 +72,9 @@ require("lazy").setup({
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("plugins.telescope")
+    end,
   },
   {
     "numToStr/Comment.nvim",
