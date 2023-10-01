a5cc139bfc22501f1ca7f2e3e15cda7c04680059
auvred <aauvred@gmail.com>
2023-10-01 08:11:30 +0000

    <leader>E -> full screen nvim-tree ; bump plugins

--- configs/nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ configs/nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -6,9 +6,9 @@ require("nvim-tree").setup({
     float = {
       enable = true,
       open_win_config = {
-        border = "double"
-      }
-    }
+        border = "double",
+      },
+    },
   },
   git = {
     ignore = false,
@@ -16,25 +16,24 @@ require("nvim-tree").setup({
 })
 
 local function open_nvim_tree(data)
-  -- buffer is a [No Name]
   local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-  -- buffer is a directory
   local directory = vim.fn.isdirectory(data.file) == 1
 
   if not no_name and not directory then
     return
   end
 
-  -- change to the directory
   if directory then
     vim.cmd.cd(data.file)
   end
 
-  -- open the tree
   require("nvim-tree.api").tree.open()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
+map("n", "<leader>E", function()
+  require("nvim-tree.api").tree.toggle({ current_window = true })
+end, { silent = true })
